d9402fc8ad1d883609479b3f3d931a4ae212e42d
Omkar <omkarpro750@gmail.com>
2023-06-04 12:43:51 +0530

    Final commit

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/[Omkars-World]/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: "/vite-deploy/"
 })
